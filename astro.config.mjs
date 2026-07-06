50d34c5d0702cc0c1c8d9a5325fd29573028272d
jljefreylooi <jl.jefreylooi@icloud.com>
2026-07-06 10:24:42 +0800

    chore: switch to noop image service for node 24
    Reorder integrations and add image configuration.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
-import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://astrofy-template.netlify.app',
-  integrations: [mdx(), sitemap(), tailwind()]
+	site: "https://astrofy-template.netlify.app",
+	integrations: [mdx(), tailwind(), sitemap()],
+	image: {
+		service: { entrypoint: 'astro/assets/services/noop' },
+	},
 });
